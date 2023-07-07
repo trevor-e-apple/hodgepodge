53498566e1c2a2e127ff25504a478236e16272dd
Trevor Apple <trevorapple@Trevors-MacBook-Air.local>
2023-07-06 22:29:16 -0700

    starting work on scanner

---
+++ src/main.rs
@@ -0,0 +1,61 @@
+use std::{fs::File, env, io::Read, todo};
+
+enum Token {
+    LScope,
+    RScope,
+    Add,
+    Minus,
+    Multiply,
+    Divide,
+    Lparen,
+    RParen,
+    BlockComment,
+    Number(i32),
+    Variable(String),
+}
+
+// TODO: add more terminals (e.g. newline)
+const TERMINAL: char = ' ';
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let path = &args[0];
+
+    // TODO: handle errors
+    let mut file = File::open(path).unwrap();
+    let mut contents = String::new();
+    // TODO: handle errors
+    file.read_to_string(&mut contents).unwrap();
+
+    // start scanning for tokens
+    let mut tokens: Vec<Token> = Vec::new();
+    let mut scan_iter = contents.chars().enumerate();
+    loop {
+        let (index, character) = match scan_iter.next() {
+            Some(value) => value,
+            None => break,
+        };
+
+        let token = if character == '+' {
+            Token::Add
+        } else if character == '-' {
+            Token::Minus
+        } else if character.is_digit(10) {
+            Token::Number(character.to_digit(10).unwrap() as i32)
+        } else if character.is_alphabetic() {
+            // TODO: handle errors
+            let (mut check_index, mut check_char) = scan_iter.next().unwrap();
+            while check_char.is_alphabetic() {
+                (check_index, check_char) = scan_iter.next().unwrap();
+                // TODO: handle errors
+            }
+            Token::Variable(contents[index..check_index].to_string())
+        } else if character == TERMINAL {
+            break;
+        } else {
+            todo!("error handling");
+            break;
+        };
+        tokens.push(token);
+    }
+}

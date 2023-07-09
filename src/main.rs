8993914038123fa4758596f3329064e8b1ab5b1f
Trevor Apple <trevorapple@Trevors-MacBook-Air.local>
2023-07-08 23:52:15 -0700

    some work on making test cases for the scanner

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-use std::{env, fs::File, io::Read, todo};
+use std::{collections::VecDeque, env, fs::File, io::Read, todo};
 
+#[derive(Debug)]
 enum Token {
     LScope,
     RScope,
@@ -18,21 +19,13 @@ enum Token {
 // TODO: add more terminals (e.g. newline)
 const TERMINAL: char = ' ';
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
-    let path = &args[1];
-
-    // TODO: handle errors
-    let mut file = File::open(path).unwrap();
-    let mut contents = String::new();
-    // TODO: handle errors
-    file.read_to_string(&mut contents).unwrap();
-
+fn scanner(contents: &str) -> Vec<Token> {
     // start scanning for tokens
     let mut tokens: Vec<Token> = Vec::new();
-    let mut scan_iter = contents.chars().enumerate();
+    let mut chars: VecDeque<char> = contents.chars().collect();
+
     loop {
-        let (index, character) = match scan_iter.next() {
+        let character = match chars.pop_front() {
             Some(value) => value,
             None => break,
         };
@@ -49,16 +42,50 @@ fn main() {
             Token::Number(character.to_digit(10).unwrap() as i32)
         } else if character.is_alphabetic() {
             // TODO: handle errors
-            let (mut check_index, mut check_char) = scan_iter.next().unwrap();
-            while check_char.is_alphabetic() {
-                (check_index, check_char) = scan_iter.next().unwrap();
+            let mut index = 0;
+            loop {
                 // TODO: handle errors
+                let check_char = chars.pop_front().unwrap();
+                if check_char.is_alphabetic() {
+                    index += 1;
+                } else {
+                    chars.push_front(check_char);
+                    break;
+                }
             }
-            Token::Variable(contents[index..check_index].to_string())
+            let string = contents[0..index].to_string();
+            Token::Variable(string)
         } else {
             todo!("error handling");
             break;
         };
         tokens.push(token);
     }
+
+    return tokens;
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let path = &args[1];
+
+    // TODO: handle errors
+    let mut file = File::open(path).unwrap();
+    let mut contents = String::new();
+    // TODO: handle errors
+    file.read_to_string(&mut contents).unwrap();
+
+    scanner(&contents);
+}
+
+#[test]
+fn one_char_var() {
+    let contents = "a + b ";
+    scanner(contents);
+}
+
+#[test]
+fn multi_char_var() {
+    let contents = "alice + bob ";
+    scanner(contents);
 }

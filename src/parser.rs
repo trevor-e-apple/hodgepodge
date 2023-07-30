b47351afc33173b8e55dedeebb2487b391733336
Trevor Apple <trevorapple@Trevors-MacBook-Air.local>
2023-07-29 18:15:59 -0700

    more work on parser and recursive descent

--- src/parser.rs
+++ src/parser.rs
@@ -6,12 +6,12 @@ use crate::scanner::Token;
 pub enum SyntaxTreeNodeType {
     #[default]
     Expression,
-    Equality(Token),
-    Comparison(Token),
-    Term(Token),
-    Factor(Token),
-    Unary(Token),
-    Primary(Token),
+    Equality(Option<Token>),
+    Comparison(Option<Token>),
+    Term(Option<Token>),
+    Factor(Option<Token>),
+    Unary(Option<Token>),
+    Primary(Option<Token>),
 }
 
 #[derive(Default)]
@@ -22,7 +22,13 @@ pub struct SyntaxTreeNode {
 
 #[derive(Default)]
 pub struct SyntaxTree {
-    tree_data: Vec<SyntaxTreeNode>,
+    root: SyntaxTreeNode,
+}
+
+struct QueueEntry<'a> {
+    node: &'a SyntaxTreeNode,
+    start_index: usize,
+    end_index: usize,
 }
 
 /// A parser that takes a Vec of Tokens and produces a tree reflecting
@@ -38,19 +44,12 @@ pub struct SyntaxTree {
 /// unary -> (("!" | "-") unary) | primary;
 /// primary -> NUMBER | STRING | "true" | "false" | ("(" expression ")");
 pub fn parse(tokens: Vec<Token>) -> SyntaxTree {
-    struct QueueEntry<'a> {
-        node: &'a SyntaxTreeNode,
-        start_index: usize,
-        end_index: usize,
-    }
-
     let mut syntax_tree = SyntaxTree { ..Default::default() };
-    let root = SyntaxTreeNode {
+    syntax_tree.root = SyntaxTreeNode {
         node_type: SyntaxTreeNodeType::Expression,
         children: vec![],
     };
-    let root_ref = &root;
-    syntax_tree.tree_data.push(root);
+    let root_ref = &syntax_tree.root;
 
     let mut queue = vec![QueueEntry {
         node: root_ref,
@@ -58,15 +57,40 @@ pub fn parse(tokens: Vec<Token>) -> SyntaxTree {
         end_index: tokens.len(),
     }];
     loop {
+        /*
+        nodes are popped from the queue. their child nodes are then found. Nodes
+        are added to the parent's children when they are spawned,
+        not when they are popped off the queue
+        */
+
         let node_entry = match queue.pop() {
             Some(value) => value,
             None => break,
         };
 
-        let node = node_entry.node;
+        let mut node = node_entry.node;
         match node.node_type {
             SyntaxTreeNodeType::Expression => {
-                // equality expansion
+                let mut child_node = SyntaxTreeNode {
+                    node_type: SyntaxTreeNodeType::Equality(None),
+                    children: vec![],
+                };
+
+                queue.push(QueueEntry { 
+                    node: &child_node,
+                    start_index: node_entry.start_index,
+                    end_index: node_entry.end_index,
+                });
+                node.children.push(&child_node);
+            }
+            SyntaxTreeNodeType::Equality(_) => {
+                let mut child_node = SyntaxTreeNode {
+                    node_type: SyntaxTreeNodeType::Comparison(None),
+                    children: vec![],
+                };
+
+                let mut equality_index: Option<usize> = None;
+
                 for index in node_entry.start_index..node_entry.end_index {
                     let token = match tokens.get(index) {
                         Some(token) => token,
@@ -74,20 +98,51 @@ pub fn parse(tokens: Vec<Token>) -> SyntaxTree {
                     };
                     match token {
                         Token::Equivalence | Token::NotEqual => {
-                            let child_node = SyntaxTreeNode {
-                                node_type: SyntaxTreeNodeType::Equality(*token),
-                                children: vec![],
-                            };
+                            equality_index = Some(index);
+                            node.node_type = SyntaxTreeNodeType::Equality(Some(*token));
+                            break;
                         }
                         _ => {}
                     }
                 }
 
-                // SyntaxTreeNode { node_type: SyntaxTreeNodeType::Equality(), children: todo!() };
-                // queue.push(value);
-            }
-            SyntaxTreeNodeType::Equality(_) => {
-                todo!();
+                match node.node_type {
+                    SyntaxTreeNodeType::Equality(_) => {
+                        match equality_index {
+                            Some(equality_index) => {
+                                // add LHS and RHS to queue
+                                let lhs = SyntaxTreeNode {
+                                    node_type: SyntaxTreeNodeType::Comparison(None),
+                                    children: vec![]
+                                };
+                                queue.push(QueueEntry { 
+                                    node: &lhs,
+                                    start_index: node_entry.start_index,
+                                    end_index: equality_index }
+                                );
+
+                                let rhs = SyntaxTreeNode {
+                                    node_type: SyntaxTreeNodeType::Equality(None),
+                                    children: vec![]
+                                };
+                                queue.push(
+                                    QueueEntry {
+                                        node: &rhs,
+                                        start_index: equality_index + 1,
+                                        end_index: node_entry.end_index
+                                    }
+                                );
+
+                                // add lhs and rhs to node children
+                            },
+                            None => todo!(),
+                        };
+                    },
+                    _ => {
+                        // error path
+                        todo!()
+                    },
+                }
             }
             SyntaxTreeNodeType::Comparison(_) => todo!(),
             SyntaxTreeNodeType::Term(_) => todo!(),
@@ -95,8 +150,6 @@ pub fn parse(tokens: Vec<Token>) -> SyntaxTree {
             SyntaxTreeNodeType::Unary(_) => todo!(),
             SyntaxTreeNodeType::Primary(_) => todo!(),
         };
-
-        todo!();
     }
 
     syntax_tree
